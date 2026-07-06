f9b8d8740eaf60e5d95dff24bcb9482260a59f1c
Mike Abbott <furrysalamander@gmail.com>
2026-07-05 21:34:46 -0600

    M6: Events service + WS-Notification pull-points/subscription client
    - Enhanced onvif/events client: PullMessages, Seek, SetSynchronizationPoint,
      CreatePullPoint, Renew, Unsubscribe
    - Uses generated tev/wsn/wsnt types for full WS-Notification support

--- onvif/events/events.go
+++ onvif/events/events.go
@@ -1,7 +1,11 @@
 package events
 
 import (
+	"time"
+
+	"github.com/furrysalamander/onvif-go/onvif/schema/core"
 	"github.com/furrysalamander/onvif-go/onvif/schema/tev"
+	"github.com/furrysalamander/onvif-go/onvif/schema/wsn"
 	"github.com/furrysalamander/onvif-go/onvif/soaphdr"
 )
 
@@ -24,5 +28,50 @@ func (c *Client) GetEventProperties() (*tev.GetEventPropertiesResponse, error) {
 func (c *Client) CreatePullPointSubscription() (*tev.CreatePullPointSubscriptionResponse, error) {
 	res := &tev.CreatePullPointSubscriptionResponse{}
 	err := c.c.Do(actionBase+"/CreatePullPointSubscription", &tev.CreatePullPointSubscription{}, res)
+	return res, err
+}
+
+func (c *Client) PullMessages(timeout time.Duration, messageLimit int) (*tev.PullMessagesResponse, error) {
+	res := &tev.PullMessagesResponse{}
+	err := c.c.Do(actionBase+"/PullMessages", &tev.PullMessages{
+		Timeout:      &core.Duration{Minutes: int(timeout.Minutes())},
+		MessageLimit: int32(messageLimit),
+	}, res)
+	return res, err
+}
+
+func (c *Client) Seek(utcTime time.Time, reverse bool) (*tev.SeekResponse, error) {
+	res := &tev.SeekResponse{}
+	req := &tev.Seek{UtcTime: utcTime}
+	if reverse {
+		req.Reverse = &reverse
+	}
+	err := c.c.Do(actionBase+"/Seek", req, res)
+	return res, err
+}
+
+func (c *Client) SetSynchronizationPoint() (*tev.SetSynchronizationPointResponse, error) {
+	res := &tev.SetSynchronizationPointResponse{}
+	err := c.c.Do(actionBase+"/SetSynchronizationPoint", &tev.SetSynchronizationPoint{}, res)
+	return res, err
+}
+
+func (c *Client) CreatePullPoint() (*wsn.CreatePullPointResponse, error) {
+	res := &wsn.CreatePullPointResponse{}
+	err := c.c.Do("http://docs.oasis-open.org/wsn/bw-2/CreatePullPoint", &wsn.CreatePullPoint{}, res)
+	return res, err
+}
+
+func (c *Client) Renew(terminationTime time.Time) (*wsn.RenewResponse, error) {
+	res := &wsn.RenewResponse{}
+	err := c.c.Do("http://docs.oasis-open.org/wsn/bw-2/Renew", &wsn.Renew{
+		TerminationTime: wsn.AbsoluteOrRelativeTimeType(terminationTime),
+	}, res)
+	return res, err
+}
+
+func (c *Client) Unsubscribe() (*wsn.UnsubscribeResponse, error) {
+	res := &wsn.UnsubscribeResponse{}
+	err := c.c.Do("http://docs.oasis-open.org/wsn/bw-2/Unsubscribe", &wsn.Unsubscribe{}, res)
 	return res, err
 }
